cad653d62ccbfe7c827168936a59dd42901a4afe
Jamie Collins <jamie@wearearch.com>
2024-05-15 13:43:16 +0100

    Removing unused files

--- README.md
+++ README.md
@@ -7,53 +7,17 @@ Extract paths, operations, parameters, schemas etc from OpenAPI/Swagger definiti
 Works with OpenAPI/Swagger 2.0 and 3.x definitions.
 
 ```
-Usage: openapi-extract [options] {infile} [{outfile}]
+Usage: openapi-extract [options] {infile}
 
 Options:
   -h, --help             Show help                                     [boolean]
-  --version              Show version number                           [boolean]
-  --openai               make the definition OpenAI compliant          [boolean]
-  --server               include server information                    [boolean]
-  --shard                shard the input to an output directory         [string]
   -p, --path             the path to extract                            [string]
-  -o, --operationid      the operationIds to extract                     [array]
-  -m, --method           the method to extract for the given path       [string]
   -i, --info             copy full info object, otherwise minimal      [boolean]
-  -d, --removeDocs       remove all externalDocs properties            [boolean]
-  -r, --removeExamples   remove all example/examples properties        [boolean]
-  -x, --removeExtensions remove all x- extension properties            [boolean]
-  -s, --security         include security information                  [boolean]
-  -v, --verbose          increase verbosity                            [boolean]
 ```
 
-or
-
-```javascript
-const openapiExtractor = require('openapi-extract');
-const options = {};
-// options.path = '...';
-// options.method = '...';
-// options.operationid = ['...'];
-const res = openapiExtractor.extract(obj, options);
-
-const map = openapiExtractor.shard(obj, options);
-```
+Stripped version of the openapi-extract package <https://github.com/Mermade/openapi-extract>. Our only use is to take the openapi documentation path names, to allow a comparison to laravel artisan routes.
 
 The `options` object takes the same values as the CLI, for these keys and default values:
 
 *   path = ''
-*   method = ''
 *   info = false
-*   openai = false
-*   removeDocs = false
-*   removeExamples = false
-*   removeExtensions = false
-*   server = false
-*   security = false
-*   operationid = []
-
-## OpenAI compliant mode
-
-This option turns on the following rules:
-
-1. The `description` properties must have a maximum length of 300 characters
--- openapi-extract.js
+++ openapi-extract.js
@@ -3,90 +3,41 @@
 'use strict';
 
 const fs = require('node:fs');
-const path = require('node:path');
 
 const yaml = require('yaml');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
 
 const extractor = require('./index.js');
 
 let argv = require('yargs')
-    .usage('Usage: openapi-extract [options] {infile} [{outfile}]')
-    .demand(1)
-    .strict()
-    .help('h')
-    .alias('h', 'help')
-    .version()
-    .string('path')
-    .alias('p','path')
-    .describe('path','the path to extract')
-    .boolean('openai')
-    .describe('openai','make definition OpenAI compliant')
-    .array('operationid')
-    .alias('o','operationid')
-    .describe('operationid','the operationIds to extract')
-    .string('method')
-    .alias('m','method')
-    .describe('method','the method to extract for the given path')
-    .boolean('info')
-    .alias('i','info')
-    .describe('info','copy full info object, otherwise minimal')
-    .boolean('server')
-    .describe('server','include server information')
-    .boolean('security')
-    .alias('s','security')
-    .describe('security','include security information')
-    .boolean('removeDocs')
-    .alias('d','removeDocs')
-    .describe('removeDocs','remove all externalDocs properties')
-    .boolean('removeExamples')
-    .alias('r','removeExamples')
-    .describe('removeExamples','remove all example/examples properties')
-    .boolean('removeExtensions')
-    .alias('x','removeExtensions')
-    .describe('removeExtensions','remove all x- extension properties')
-    .string('shard')
-    .describe('shard','shard the input to an output directory')
-    .boolean('verbose')
-    .alias('v','verbose')
-    .describe('verbose','increase verbosity')
-    .argv;
+  .usage('Usage: openapi-extract [options] {infile}')
+  .demand(1)
+  .strict()
+  .help('h')
+  .alias('h', 'help')
+  .version()
+  .string('path')
+  .alias('p', 'path')
+  .describe('path', 'the path to extract')
+  .boolean('info')
+  .alias('i', 'info')
+  .describe('info', 'copy full info object, otherwise minimal')
+  .argv;
 
 async function main() {
-  let s = fs.readFileSync(argv._[0],'utf8');
+  let s = fs.readFileSync(argv._[0], 'utf8');
   let obj = yaml.parse(s);
-  let filename = 'openapi.yaml';
-  if (obj.asyncapi) filename = 'asyncapi.yaml';
-  if (argv.shard) {
-      const map = extractor.shard(obj, argv);
-      await rimraf(argv.shard, { preserveRoot: false });
-      for (let [key, value] of map) {
-          process.stderr.write('.');
-          try {
-            await mkdirp(path.resolve(argv.shard,key));
-            fs.writeFileSync(path.resolve(argv.shard,key,filename),yaml.stringify(value),'utf8');
-          }
-          catch (ex) {
-            process.stderr.write(`\n${ex.message}\n`);
-          }
-      }
-      process.stderr.write('\n');
+  let res = extractor.extract(obj, argv);
+  if (argv._[0].indexOf('.json') >= 0) {
+    s = JSON.stringify(res, null, 2);
   }
   else {
-    let res = extractor.extract(obj, argv);
-    if (argv._[0].indexOf('.json')>=0) {
-        s = JSON.stringify(res,null,2);
-    }
-    else {
-        s = yaml.stringify(res);
-    }
-    if (argv._.length>1) {
-        fs.writeFileSync(argv._[1],s,'utf8');
-    }
-    else {
-        console.log(s);
-    }
+    s = yaml.stringify(res);
+  }
+  if (argv._.length > 1) {
+    console.log(s);
+  }
+  else {
+    console.log(s);
   }
 }
 
