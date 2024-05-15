a49674984e478f190d0c9e57ee882ec824b1b6fa
Jamie Collins <jamie@wearearch.com>
2024-05-15 13:40:09 +0100

    Stripping readme from the arguments not needed anymore

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function deref(target, src, obj) {
     let seen = {};
     while (changes) {
         changes = 0;
-        recurse(target,{},function(t,key,state){
+        recurse(target, {}, function (t, key, state) {
             if ((key === '$ref') && (typeof t[key] === 'string') && !seen[state.path]) {
                 if (t[key].startsWith('#')) {
                     changes++;
-                    jptr(src,t[key],jptr(obj,t[key]));
+                    jptr(src, t[key], jptr(obj, t[key]));
                 }
                 seen[state.path] = true;
             }
@@ -43,7 +43,7 @@ function extract(obj, options) {
     defaults.security = false;
     defaults.openai = false;
     defaults.operationid = [];
-    options = Object.assign({},defaults,options);
+    options = Object.assign({}, defaults, options);
 
     if (!Array.isArray(options.operationid)) {
         options.operationid = [options.operationid];
@@ -110,7 +110,7 @@ function extract(obj, options) {
                     if (op.operationId && op.operationId === id) {
                         if (!paths[p]) paths[p] = {};
                         paths[p][o] = clone(op);
-                        deref(paths[p][o],src,obj);
+                        deref(paths[p][o], src, obj);
                     }
                 }
             }
@@ -122,7 +122,7 @@ function extract(obj, options) {
         if (options.method) paths[options.path][options.method] = {};
         if (options.method && obj.paths[options.path][options.method]) {
             paths[options.path][options.method] = clone(obj.paths[options.path][options.method]);
-            deref(paths[options.path][options.method],src,obj);
+            deref(paths[options.path][options.method], src, obj);
         }
         else if (options.path) {
             for (let o in obj.paths[options.path]) {
@@ -130,12 +130,17 @@ function extract(obj, options) {
                     (!o.startsWith('x-'))) {
                     if (!options.method || options.method === o) {
                         paths[options.path][o] = clone(obj.paths[options.path][o]);
-                        deref(paths[options.path][o],src,obj);
+                        deref(paths[options.path][o], src, obj);
                     }
                 }
             }
         }
     }
+
+    paths = obj.paths;
+    let pathKeys = {};
+    pathKeys = Object.keys(paths);
+    console.log(pathKeys);
 
     if (paths) src.paths = paths;
     for (let p in paths) {
@@ -145,7 +150,7 @@ function extract(obj, options) {
     }
 
     if (options.server && (Object.keys(paths).length === 1) &&
-      (Object.keys(Object.values(paths)[0]) === 1)) {
+        (Object.keys(Object.values(paths)[0]) === 1)) {
         const op = Object.values(Object.values(paths)[0])[0];
         if (op.schemes) {
             src.schemes = op.schemes;
@@ -157,47 +162,47 @@ function extract(obj, options) {
         }
     }
 
-    deref(src.definitions,src,obj);
-    deref(src.headers,src,obj);
-    deref(src.responses,src,obj);
-    deref(src.parameters,src,obj);
-    deref(src.components,src,obj);
+    deref(src.definitions, src, obj);
+    deref(src.headers, src, obj);
+    deref(src.responses, src, obj);
+    deref(src.parameters, src, obj);
+    deref(src.components, src, obj);
 
     if (options.openai) {
-        recurse(src,{},function(obj,key,state){
-          if (obj && key === 'description' && obj.description.length > 300) {
-              state.parent[state.pkey].description = obj.description.substring(0,300);
-          }
+        recurse(src, {}, function (obj, key, state) {
+            if (obj && key === 'description' && obj.description.length > 300) {
+                state.parent[state.pkey].description = obj.description.substring(0, 300);
+            }
         });
     }
 
-    clean(src,'paths');
-    clean(src,'definitions');
-    clean(src,'headers');
-    clean(src,'responses');
-    clean(src,'parameters');
-    clean(src.components,'parameters');
-    clean(src.components,'responses');
-    clean(src.components,'headers');
-    clean(src.components,'schemas');
-    clean(src,'components');
+    clean(src, 'paths');
+    clean(src, 'definitions');
+    clean(src, 'headers');
+    clean(src, 'responses');
+    clean(src, 'parameters');
+    clean(src.components, 'parameters');
+    clean(src.components, 'responses');
+    clean(src.components, 'headers');
+    clean(src.components, 'schemas');
+    clean(src, 'components');
 
     const al = new AList(src);
     if (options.removeExamples) {
-        for (let [value,parents] of al) {
+        for (let [value, parents] of al) {
             AList.deleteProperty(value, 'example');
             AList.deleteProperty(value, 'examples');
         }
     }
 
     if (options.removeExtensions) {
-        for (let [value,parents] of al) {
+        for (let [value, parents] of al) {
             AList.deletePrefix(value, 'x-');
         }
     }
 
     if (options.removeDocs) {
-        for (let [value,parents] of al) {
+        for (let [value, parents] of al) {
             AList.deleteProperty(value, 'externalDocs');
         }
     }
@@ -209,21 +214,21 @@ function shard(obj, options) {
     const results = new Map();
     if (!options.operationid) options.operationid = [];
     if (typeof options.operationid === 'string') {
-        options.operationid = [ options.operationid ];
+        options.operationid = [options.operationid];
     }
     for (let path in obj.paths) {
         if (!options.path || options.path === path) {
             for (let method in obj.paths[path]) {
                 if (!options.method || options.method === method) {
                     if (!obj.paths[path][method].operationId || options.operationid.length === 0 || (options.operationid && options.operationid.indexOf(obj.paths[path][method].operationId) >= 0)) {
-                        let output = extract(obj, Object.assign({},options,{ path, method, operationid: obj.paths[path][method].operationId }));
+                        let output = extract(obj, Object.assign({}, options, { path, method, operationid: obj.paths[path][method].operationId }));
                         if (output.paths && Object.keys(output.paths).length > 0) {
                             let key = obj.paths[path][method].operationId;
-                            if (!key) key = method+'-'+path;
+                            if (!key) key = method + '-' + path;
                             key = key.split('/').join('-').split('--').join('-').split('{').join('').split('}').join('');
                             results.set(key, output);
 
-                      }
+                        }
                     }
                 }
             }
